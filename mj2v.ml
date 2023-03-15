b38a5d4b7c1e7af3b6b8c1a958dca2b3e0434a14
AndreiCocan <95496161+AndreiCocan@users.noreply.github.com>
2023-03-15 15:07:13 +0100

    Trying to make "this " work

--- mj2v.ml
+++ mj2v.ml
@@ -16,53 +16,67 @@ let binop2v = function
   | OpLt  -> sprintf "<"
   | OpAnd -> sprintf "&&"
 
-  let rec expr0 () = function
+let var2v () (var_name, class_attribute) =
+try 
+StringMap.find var_name class_attribute;
+sprintf "this.%s" var_name;
+with Not_found ->  sprintf "%s" var_name
+
+let rec expr0 () (e,class_attribute) =
+  match e with
   | EConst const -> sprintf  "%a" constant2v const
-  | EGetVar var -> sprintf "%s" var
+  | EGetVar var -> var2v  "%s" (var,class_attribute)
   | EThis -> "this"
-  | EMethodCall (java_object,method_name,args) -> sprintf "%a.%s(%a)" expr0 java_object method_name (seplist comma expression2v) args
-  | EArrayGet (array,index) -> sprintf "%a[%a]" expr0 array expression2v index
-  | EArrayLength array -> sprintf "%a.len" expr0 array
+  | EMethodCall (java_object,method_name,args) -> sprintf "%a.%s(%a)" expr0 (java_object,class_attribute) method_name (seplist comma expression2v) (args,class_attribute)
+  | EArrayGet (array,index) -> sprintf "%a[%a]" expr0 (array,class_attribute) expression2v (index,class_attribute)
+  | EArrayLength array -> sprintf "%a.len" expr0 (array,class_attribute)
   | EObjectAlloc class_name -> sprintf "%s{}" class_name
-  | e -> sprintf "(%a)" expression2v e
-and expr1 () = function
-  | EArrayAlloc size -> sprintf "[]int{len: %a, init: 0}" expression2v size
-  | e -> expr0 () e
+  | e -> sprintf "(%a)" expression2v (e,class_attribute)
 
-and expr2 () = function
-  | EUnOp (UOpNot, e) -> sprintf "!%a" expr2 e
-  | e -> expr1 () e
+and expr1 ()  (e,class_attribute)=
+  match e with
+  | EArrayAlloc size -> sprintf "[]int{len: %a, init: 0}" expression2v (size,class_attribute)
+  | e -> expr0 () (e,class_attribute)
 
-and expr3 () = function
-  | EBinOp (OpMul as op, e1, e2) -> sprintf "%a %s %a" expr3 e1 (binop2v op) expr3 e2
-  | e -> expr2 () e
+and expr2 ()  (e,class_attribute)=
+  match e with
+  | EUnOp (UOpNot, e) -> sprintf "!%a" expr2 (e,class_attribute)
+  | e -> expr1 () (e,class_attribute)
 
-and expr4 () = function
-  | EBinOp (OpSub as op, e1, e2) -> sprintf "%a %s %a" expr4 e1 (binop2v op) expr3 e2
-  | e -> expr3 () e
+and expr3 ()  (e,class_attribute)= 
+  match e with
+  | EBinOp (OpMul as op, e1, e2) -> sprintf "%a %s %a" expr3 (e1,class_attribute) (binop2v op) expr3 (e2,class_attribute)
+  | e -> expr2 () (e,class_attribute)
 
-and expr5 () = function
-  | EBinOp (OpAdd as op, e1, e2) -> sprintf "%a %s %a" expr5 e1 (binop2v op) expr5 e2
-  | e -> expr4 () e
+and expr4 () (e,class_attribute) = 
+  match e with
+  | EBinOp (OpSub as op, e1, e2) -> sprintf "%a %s %a" expr4 (e1,class_attribute) (binop2v op) expr3 (e2,class_attribute)
+  | e -> expr3 () (e,class_attribute)
 
-and expr6 () = function
-  | EBinOp ((OpLt | OpAnd) as op, e1, e2) -> sprintf "%a %s %a" expr6 e1 (binop2v op) expr6 e2
-  | e -> expr5 () e
+and expr5 () (e,class_attribute) =
+  match e with
+  | EBinOp (OpAdd as op, e1, e2) -> sprintf "%a %s %a" expr5 (e1,class_attribute) (binop2v op) expr5 (e2,class_attribute)
+  | e -> expr4 () (e,class_attribute)
 
-and expression2v () e = expr6 () e
+and expr6 () (e,class_attribute) = 
+  match e with
+  | EBinOp ((OpLt | OpAnd) as op, e1, e2) -> sprintf "%a %s %a" expr6 (e1,class_attribute) (binop2v op) expr6 (e2,class_attribute)
+  | e -> expr5 () (e,class_attribute)
+
+and expression2v () (e,class_attribute) = expr6 () (e,class_attribute)
 
 
-let rec statement2v out stat = 
+let rec statement2v () (stat,class_attribute) = 
   match stat with
-  | SBlock statements -> sprintf"{%a%t}" (indent indentation (seplist nl statement2v)) statements nl
+  | SBlock statements -> sprintf"{%a%t}" (indent indentation (seplist nl statement2v)) (statements,class_attribute) nl
   | SIf(ex,stmnt1,stmnt2) -> sprintf "if %a {\n%a%t\n} else {\n%a\n}" 
     (expression2v) ex
-    statement2v stmnt1
+    statement2v (stmnt1,class_attribute)
     nl
-    statement2v stmnt2
-  | SWhile (ex, stmnt) -> sprintf "for %a {\n%a\n}" expression2v ex statement2v stmnt
+    statement2v (stmnt2,class_attribute)
+  | SWhile (ex, stmnt) -> sprintf "for %a {\n%a\n}" expression2v ex statement2v (stmnt,class_attribute)
   | SSysou ex -> sprintf "fmt.Println(%a)" expression2v ex
-  | SSetVar (var,ex) -> sprintf "%s = %a" var expression2v ex
+  | SSetVar (var,ex) -> sprintf "%a = %a" var2v var expression2v ex
   | SArraySet (array, index, ex) -> sprintf "%s[%a] = %a"
     array
     expression2v index
@@ -94,7 +108,7 @@ let method2v () (method_name, m, class_name,java_class)  =
     (seplist comma decl2v) m.arguments
     java_type2v m.return_type
     (termlist nl (indent indentation decl_var2v)) (StringMap.to_association_list m.method_declarations)
-    (list (indent indentation statement2v)) m.method_statements
+    (list (indent indentation statement2v)) (List.map (fun (x, y) -> (x, y, java_class.attributes)) m.method_statements)
     (indent indentation return2v) m.return_expression
 
 let class2v () (class_name, java_class) =
